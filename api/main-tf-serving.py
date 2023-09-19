d1a1d03544dfdafb89518c25bb523cf06f2098b5
Anas Aamoum <anas.aamoum@gmail.com>
2023-09-19 20:49:10 +0300

    CORS errors handled

--- api/main-tf-serving.py
+++ api/main-tf-serving.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, UploadFile, File
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 import numpy as np
 import requests
@@ -6,6 +7,19 @@ import requests
 from utils import read_file_as_image
 
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 model_verison = 6
 
